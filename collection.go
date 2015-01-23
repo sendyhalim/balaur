9c4b407a1ed1ddb69c4c2163b2494c18c333c5d1
sendyHalim <sendy_ubm@yahoo.com>
2015-01-23 13:22:43 +0700

    Add TotalApps()

--- collection.go
+++ collection.go
@@ -1,15 +1,19 @@
 package balaur
 
-var collection = map[string]*App{}
+var appCollection = map[string]*App{}
 
 func AddApp(app *App) {
-	collection[app.Name] = app
+	appCollection[app.Name] = app
 }
 
 func GetApp(name string) *App {
-	return collection[name]
+	return appCollection[name]
 }
 
 func RemoveApp(name string) {
-	delete(collection, name)
+	delete(appCollection, name)
+}
+
+func TotalApps() int {
+	return len(appCollection)
 }
